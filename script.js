1f161994b5875cca2a848479a05b049e0fb78061
X-CYL <castellastro@gmail.com>
2023-05-06 14:10:27 +0200

    building the script file reset button and font style

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 let calcButton = document.getElementById("calculer");
 let vider = document.getElementById("clear");
+let error = "Vous n'avez pas rempli les champs de formulaire"
+//let iconError = <i class="bi bi-emoji-frown"></i>;
 
-//recupérer les valeurs des champs inputs dans la variablr "formCalc"
-
+//griser le champ input vide (empecher la saisie) lorsque 2 champs sont remplis
+function resetForm() {
+    document.getElementById("inputForm").reset();
+    document.getElementById("result").innerText = "";
+  }
 
 //calculer 
 function calculFormules(){
@@ -18,24 +23,30 @@ function calculFormules(){
     let calculTension = r * i;
     let calculResistance = u / i;
     let calculIntensite = u / r;
-    document.getElementById('result').innerText = calculIntensite.toFixed(2) +'A'; // exemple
 
     //on mets les conditions de calculs selon les champs remplis
     if(r !== '' && i !== ''){
-        document.getElementById('result').innerText = calculTension.toFixed(2) +'Volts';
+        document.getElementById('result').innerText = calculTension.toFixed(2);
+        document.getElementById('unit').innerText = "Volts";
     }
     else if(u !== '' && i !== ''){
-        document.getElementById('result').innerText = calculResistance.toFixed(2) +'Ohms';
+        document.getElementById('result').innerText = calculResistance.toFixed(2);
+        document.getElementById('unit').innerText = "Ohms";
     }
     else if(u !== '' && r !== ''){
-        document.getElementById('result').innerText = calculIntensite.toFixed(2) +'Amps';
+        document.getElementById('result').innerText = calculIntensite.toFixed(2);
+        document.getElementById('unit').innerText = "Ampères";
     }
     else{
-        alert("vous n'avez pas bien rempli le formulaire");
-        document.getElementById('result').innerText = 'Null';
+        //alert("vous n'avez pas bien rempli le formulaire");
+        document.getElementById("result").innerText = "####";
+        document.getElementById('unit').innerText = error;
+        document.getElementById('unit').style.color = "red";
     }
     
 }
+vider.addEventListener('click', resetForm);
+calcButton.addEventListener('click',calculFormules)
 
 //affecter la fonction de calcul au bouton "calculer"
 
