f4d936989d749e56c92a3f7345b72f3bcb333fea
X-CYL <castellastro@gmail.com>
2023-08-08 22:44:17 +0200

    fixed problem calcul function

--- script.js
+++ script.js
@@ -3,32 +3,30 @@ let vider = document.getElementById("clear");
 let error = "Vous n'avez pas rempli les champs de formulaire";
 //let iconError = <i class="bi bi-emoji-frown"></i>;
 
-
 //griser le champ input vide (empecher la saisie) lorsque 2 des 3 champs sont remplis
 
 function ctrlInputs() {
-    let u = document.getElementById("tension");
-    let i = document.getElementById("intensite");
-    let r = document.getElementById("resistance");
+let u = document.getElementById("tension");
+let i = document.getElementById("intensite");
+let r = document.getElementById("resistance");
 
-  let disabled = addEventListener("input", function () {
-    if (u.value !== "" && r.value !== "") {
-        i.setAttribute("disabled", "");
-    } else {
-        i.removeAttribute("disabled", "");
-    }
-    if (i.value !== "" && r.value !== "") {
-      u.setAttribute("disabled", "");
-    } else {
-      u.removeAttribute("disabled", "");
-    }
-    if (u.value !== "" && i.value !== "") {
-      r.setAttribute("disabled", "");
-    } else {
-      r.removeAttribute("disabled", "");
-    }
-    
-  });
+addEventListener("input", function () {
+  if (u.value !== "" && r.value !== "") {
+    i.setAttribute("disabled", "");
+  } else {
+    i.removeAttribute("disabled", "");
+  }
+  if (i.value !== "" && r.value !== "") {
+    u.setAttribute("disabled", "");
+  } else {
+    u.removeAttribute("disabled", "");
+  }
+  if (u.value !== "" && i.value !== "") {
+    r.setAttribute("disabled", "");
+  } else {
+    r.removeAttribute("disabled", "");
+  }
+});
 }
 
 // reset des champs
@@ -46,14 +44,16 @@ ctrlInputs();
 //calculer la formule
 function calculFormules() {
   //recup du formulaire dans une variable, ici (formCalc)
-  let formCalc = document.getElementById("inputForm");
+  /*let formCalc = document.getElementById("inputForm");
   //on transforme le formulaire en objet dans lequel on peut recupérer chaque champ input par son name via get dans l'objet
   let formObj = new FormData(formCalc);
   let tension = formObj.get("tension");
   let resistance = formObj.get("resistance");
   let intensite = formObj.get("intensite");
-
-  
+*/
+let tension = document.getElementById('tension').value;
+let resistance = document.getElementById('resistance').value;
+let intensite = document.getElementById('intensite').value;
   //on lance le calcul
 
   //on mets les conditions de calculs selon les champs remplis
