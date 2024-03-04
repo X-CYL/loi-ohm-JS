d5890a2d1d95a0f31951f7e39eeaf95ce8cbcdf1
X-CYL <castellastro@gmail.com>
2024-03-04 13:05:18 +0100

    correction calcul method

--- script.js
+++ script.js
@@ -2,36 +2,46 @@ let calcButton = document.getElementById("calculer");
 let vider = document.getElementById("clear");
 let error = "Il faut remplir au moins deux champs de formulaire";
 let troisPhases = 1.732;
-console.log(troisPhases)
+let currentMono = "Courant Monophasé"
+let currentTri = "Courant Triphasé"
 let checkbox = document.getElementById("temp");
+let phaseDisplay = document.getElementById("currentSelect");
 //let iconError = <i class="bi bi-emoji-frown"></i>;
 
 //griser le champ input vide (empecher la saisie) lorsque 2 des 3 champs sont remplis
 
-function ctrlInputs() {
-let u = document.getElementById("tension");
-let i = document.getElementById("intensite");
-let r = document.getElementById("resistance");
-
-addEventListener("input", function () {
-  if (u.value !== "" && r.value !== "") {
-    i.setAttribute("disabled", "");
-  } else {
-    i.removeAttribute("disabled", "");
-  }
-  if (i.value !== "" && r.value !== "") {
-    u.setAttribute("disabled", "");
-  } else {
-    u.removeAttribute("disabled", "");
-  }
-  if (u.value !== "" && i.value !== "") {
-    r.setAttribute("disabled", "");
-  } else {
-    r.removeAttribute("disabled", "");
-  }
-});
+function byPassPhase() {
+  if (checkbox.checked == true) { 
+    calculFormule3ph();
+  } else
+    calculFormules()   
 }
 
+checkbox.addEventListener("change", byPassPhase);
+
+function ctrlInputs() {
+  let u = document.getElementById("tension");
+  let i = document.getElementById("intensite");
+  let r = document.getElementById("resistance");
+
+  addEventListener("input", function () {
+    if (u.value !== "" && r.value !== "") {
+      i.setAttribute("disabled", "");
+    } else {
+      i.removeAttribute("disabled", "");
+    }
+    if (i.value !== "" && r.value !== "") {
+      u.setAttribute("disabled", "");
+    } else {
+      u.removeAttribute("disabled", "");
+    }
+    if (u.value !== "" && i.value !== "") {
+      r.setAttribute("disabled", "");
+    } else {
+      r.removeAttribute("disabled", "");
+    }
+  });
+}
 
 // reset des champs
 function resetForm() {
@@ -41,83 +51,91 @@ function resetForm() {
   tension.removeAttribute("disabled", "");
   resistance.removeAttribute("disabled", "");
   intensite.removeAttribute("disabled", "");
+  checkbox.checked = false;
+  phaseDisplay.innerText = currentMono;
 }
 
 ctrlInputs();
 
 //calculer la formule en mono
 function calculFormules() {
-  
-let tension = document.getElementById('tension').value;
-let resistance = document.getElementById('resistance').value;
-let intensite = document.getElementById('intensite').value;
+  let tension = document.getElementById("tension").value;
+  let resistance = document.getElementById("resistance").value;
+  let intensite = document.getElementById("intensite").value;
   //on lance le calcul
 
   //on mets les conditions de calculs selon les champs remplis
   if (tension !== "" && resistance !== "") {
     let calculIntensite = tension / resistance;
     document.getElementById("result").innerText = calculIntensite.toFixed(2);
-    document.getElementById("unit").innerText = "Ampères pour l'intensité";
+    document.getElementById("unit").innerText = "Ampères pour l'intensité en mono";
   } else if (intensite !== "" && resistance !== "") {
     let calculTension = resistance * intensite;
     document.getElementById("result").innerText = calculTension.toFixed(2);
-    document.getElementById("unit").innerText = "Volts pour la tension";
+    document.getElementById("unit").innerText = "Volts pour la tension en mono";
   } else if (tension !== "" && intensite !== "") {
     let calculResistance = tension / intensite;
     document.getElementById("result").innerText = calculResistance.toFixed(2);
-    document.getElementById("unit").innerText = "Ohms pour la résistance";
+    document.getElementById("unit").innerText = "Ohms pour la résistance en mono";
   } else {
     //alert("vous n'avez pas bien rempli le formulaire");
     document.getElementById("unit").innerText = error;
     document.getElementById("unit").style.color = "red";
-    document.getElementById("result").innerHTML ="<img \src=./pics/badIcon.png\ >"
+    document.getElementById("result").innerHTML =
+      "<img src=./pics/badIcon.png >";
   }
+  phaseDisplay.innerText = currentMono;
 }
 // calcul en triphasé
 function calculFormule3ph() {
-  
-let tension = document.getElementById('tension').value;
-let resistance = document.getElementById('resistance').value;
-let intensite = document.getElementById('intensite').value;
+  let tension = document.getElementById("tension").value;
+  let resistance = document.getElementById("resistance").value;
+  let intensite = document.getElementById("intensite").value;
   //on lance le calcul
 
   //on mets les conditions de calculs selon les champs remplis
   if (tension !== "" && resistance !== "") {
     let calculIntensite = tension / (resistance * troisPhases);
     document.getElementById("result").innerText = calculIntensite.toFixed(2);
-    document.getElementById("unit").innerText = "Ampères pour l'intensité en tri";
+    document.getElementById("unit").innerText =
+      "Ampères pour l'intensité en tri";
   } else if (intensite !== "" && resistance !== "") {
     let calculTension = resistance * (intensite * troisPhases);
     document.getElementById("result").innerText = calculTension.toFixed(2);
     document.getElementById("unit").innerText = "Volts pour la tension en tri";
   } else if (tension !== "" && intensite !== "") {
-    let calculResistance = tension / (intensite *troisPhases);
+    let calculResistance = tension / (intensite * troisPhases);
     document.getElementById("result").innerText = calculResistance.toFixed(2);
-    document.getElementById("unit").innerText = "Ohms pour la résistance en tri";
+    document.getElementById("unit").innerText =
+      "Ohms pour la résistance en tri";
   } else {
     //alert("vous n'avez pas bien rempli le formulaire");
     document.getElementById("unit").innerText = error;
     document.getElementById("unit").style.color = "red";
-    document.getElementById("result").innerHTML ="<img \src=./pics/badIcon.png\ >"
+    document.getElementById("result").innerHTML =
+      "<img src=./pics/badIcon.png >";
   }
+  phaseDisplay.innerText = currentTri;
 }
 
 //calculer selon que la tension est monophasée ou triphasée
-function calculsValeurs(){
-  if(checkbox.checked){
+
+function calculsValeurs() {
+  if (checkbox.checked) {
     calculFormule3ph();
   } else {
     calculFormules();
   }
 }
+byPassPhase();
 
-calcButton.addEventListener("click", calculsValeurs);   
+calcButton.addEventListener("click", calculsValeurs);
 vider.addEventListener("click", resetForm);
 
-const leRoot = document.querySelector('#root')
+const leRoot = document.querySelector("#root");
 
 //exercice
 
 //const afficher = document.createElement('span');
-    //afficher.innerText ="IL FAUT FAIRE ATTENTION";
+//afficher.innerText ="IL FAUT FAIRE ATTENTION";
 //leRoot.appendChild(afficher);
